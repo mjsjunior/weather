151d06ebeed7e6c2ce84eab6f99e4b200567b487
Mauricio Júnior <mauricio@mjunior.com.br>
2015-09-04 01:05:11 -0300

    Alteracoes layout e funcoes

--- static/js/main.js
+++ static/js/main.js
@@ -4,16 +4,36 @@ $(document).ready(function(){
 	altura = $(document).height();
 	api = "http://api.openweathermap.org/data/2.5/";
 	endPoint = "APPID=231bdedaf245423d58c78e595650cfcb";
-	limiteCidades = 20;
+	limiteCidades = 10;
 	getLocation();
 	function getLocation() {
 	    if (navigator.geolocation) {
-	        navigator.geolocation.getCurrentPosition(function(position){
-	        	createMap(position.coords.latitude,position.coords.longitude)
-	        });
+	        navigator.geolocation.getCurrentPosition(init, showError);
 	        
 	    } else { 
-	        createMap(10,10);
+	        alert('Seu navegador não suporta geolocation');
+	        createMap(-21.22583,-43.77);
+	    }
+	}
+
+	function init(position){
+		createMap(position.coords.latitude,position.coords.longitude)
+	}
+
+	function showError(error) {
+    switch(error.code) {
+        case error.PERMISSION_DENIED:
+            alert("User denied the request for Geolocation.")
+            break;
+        case error.POSITION_UNAVAILABLE:
+            alert("Location information is unavailable.")
+            break;
+        case error.TIMEOUT:
+            alert("The request to get user location timed out.")
+            break;
+        case error.UNKNOWN_ERROR:
+            alert("An unknown error occurred.")
+            break;
 	    }
 	}
 	
@@ -54,11 +74,16 @@ $(document).ready(function(){
 	function createMap(lat,lon) {
 		    var url = api+"find?lat="+lat+"&lon="+lon+"&cnt="+limiteCidades+""+"&units=metric&"+endPoint
 		    $.get(url, function(data) {
-			  var cidade = data['name'];			  
-			  var date = new Date(data['list'][0]['sys']['sunrise']);
-			  var img = "http://openweathermap.org/img/w/"+data['list'][0]['weather'][0]['icon']+".png"
-			  montarApresentacao(lat,lon,cidade,date,img,data);
-			})
+		    	if(data['cod'] == '404')
+			 {
+				 alert('Erro ao montar o mapa...');
+			 }else{
+					  var cidade = data['name'];			  
+					  var date = new Date(data['list'][0]['sys']['sunrise']);
+					  var img = "http://openweathermap.org/img/w/"+data['list'][0]['weather'][0]['icon']+".png"
+					  montarApresentacao(lat,lon,cidade,date,img,data);
+					}
+				})
 			  
 		  .fail(function() {
 		    alert( "error" );
@@ -71,23 +96,21 @@ $(document).ready(function(){
 		var latlng = new google.maps.LatLng(lat,lon);
 	    var options = {
 	        zoom: 11,
-			mapTypeControl: true,
-			mapTypeControlOptions: {
-				style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
-				position: google.maps.ControlPosition.TOP_CENTER
-			},
-			zoomControl: true,
-			zoomControlOptions: {
-				style: google.maps.ZoomControlStyle.LARGE,
-				position: google.maps.ControlPosition.LEFT_CENTER
-			},
-			scaleControl: true,
-			streetViewControl: true,
-			streetViewControlOptions: {
-				position: google.maps.ControlPosition.LEFT_TOP
-			},
-	        center: latlng,
-	        mapTypeId: google.maps.MapTypeId.ROADMAP
+			center: latlng,
+	        mapTypeId: google.maps.MapTypeId.ROADMAP,
+	        mapTypeControl: true,
+		    mapTypeControlOptions: {
+		      style: google.maps.MapTypeControlStyle.DROPDOWN_MENU,
+		      mapTypeIds: [
+		        google.maps.MapTypeId.ROADMAP,
+		        google.maps.MapTypeId.TERRAIN
+		      ]
+		    },
+		    zoomControl: true,
+		    zoomControlOptions: {
+		      style: google.maps.ZoomControlStyle.SMALL,
+		      position: google.maps.ControlPosition.RIGHT_CENTER
+		    }
 	    };
 	    $('#mapa').css('height',altura)
 	    map = new google.maps.Map(document.getElementById("mapa"), options);
@@ -97,69 +120,81 @@ $(document).ready(function(){
 			
 			var marker = new google.maps.Marker({
 	            position: new google.maps.LatLng(cidade['coord']['lat'],cidade['coord']['lon']),
-	            labelContent: "$425K",
-		       labelAnchor: new google.maps.Point(22, 0),
-		       labelClass: "labels", // the CSS class for the label
-		       labelStyle: {opacity: 0.75},
+	     
 	            map: map,
 	            animation: google.maps.Animation.DROP,
 	            icon: img
 		    });
 
-			var conteudo = '<h5> '+cidade['name']+' </h5>'+
-			'<p>Temp: '+cidade['main']['temp_max']+'°C</p>';
-		    var infowindow = new google.maps.InfoWindow({
-			    content: conteudo
-			  });
-			  
-			  
 			
-
 		   marker.addListener('click', function() {
 				createModal(cidade);
-				toggleBounce();
+			
 		  });
 		})
 
 
 		function createModal(cidade){
-			
-			
+			$('#previsaoDias').empty();
+			var url = api+"weather?id="+cidade['id']+"&units=metric&"+endPoint
+			//var url = api+"weather?q="+cidade['nome']+"&units=metric&"+endPoint
+			$.get(url,function(data){
+				if(data['cod'] != '404')
+				{
+				 	sunrise = datahorario(new Date(data['sys']['sunrise']*1000));
+				 	sunset = datahorario(new Date(data['sys']['sunset']*1000));
+				 	name = data['name']
+				 	temp = data['main']['temp']
 
-			var url = api+"forecast/daily?lat="+cidade['coord']['lat']+"&lon="+cidade['coord']['lon']+"&units=metric&cnt=7&"+endPoint
-		    $.get(url, function(data) {
-			  var cidade = data['city'];
-			  var dias = data['list']
-			  $.each(dias,function(i,dia){
-			  		if(i == 0)
-			  		{
-			  			$('#nivelMar').text('Pressão atmosférica: '+dia['pressure'])
-			  		}
-				  	var diaHtml = '<li class="collection-item avatar">'+
-	                '<img src="${imgUrl}" alt="" class="circle">'+
-	                '<span class="title">${data}</span>'+
-	                '<p>Temp. Máx: ${tempMax} <br> Temp. Min: ${tempMin} </p>'+
-	                '<a href="#!" class="secondary-content"><i class="material-icons">grade</i></a>'+
-	              '</li>';
-			  		console.log(dataFormatada(new Date(dia['dt']*1000)));
-				  var data = new Date(dia['dt']*1000)
-				  var img = "http://openweathermap.org/img/w/"+dia['weather'][0]['icon']+".png"
-				  diaHtml = diaHtml.replace('${imgUrl}',img)
-				  diaHtml = diaHtml.replace('${data}',dataFormatada(data))
-				  diaHtml = diaHtml.replace('${tempMax}',dia['temp']['max'])
-				  diaHtml = diaHtml.replace('${tempMin}',dia['temp']['min'])
-				  $('#previsaoDias').append(diaHtml); 
-			  });
+				 	
+				 	url = api+"forecast/daily?id="+cidade['id']+"&units=metric&cnt=7&"+endPoint
+				 	//url = api+"forecast/daily?q="+cidade['name']+"&units=metric&cnt=7&"+endPoint
+				    $.get(url, function(data) {
+				    		console.log(data)
+					  var cidade = data['city'];
+					  var dias = data['list']
+						  if(data['cod'] == '404'){
+						  	$('#erro').click();
+						  }else{
+						  	  $('#nomeCidade').text(name);
+							  $.each(dias,function(i,dia){
+							  		var diaHtml = '<li class="collection-item avatar">'+
+						                '<img src="${imgUrl}" alt="" class="circle">'+
+						                '<span class="title">${data}</span>'+
+						                '<p>Temp. Máx: ${tempMax} <br> Temp. Min: ${tempMin} </p>'+
+						                ''+
+						              '</li>';
+
+							  		if(i != 0)
+							  		{  	
+								  	  console.log(dataFormatada(new Date(dia['dt']*1000)));
+									  var data = new Date(dia['dt']*1000)
+									  var img = "http://openweathermap.org/img/w/"+dia['weather'][0]['icon']+".png"
+									  diaHtml = diaHtml.replace('${imgUrl}',img)
+									  diaHtml = diaHtml.replace('${data}',dataFormatada(data))
+									  diaHtml = diaHtml.replace('${tempMax}',Math.ceil(dia['temp']['max'])+' °c')
+									  diaHtml = diaHtml.replace('${tempMin}',Math.ceil(dia['temp']['min'])+' °c')
+									 
+									  $('#previsaoDias').append(diaHtml); 
+									}
+							 	 });
+							    	$('#latlon').text('Lat: '+cidade['coord']['lat']+' ; Lon: '+cidade['coord']['lon'])
+									$('#sunrise').text(sunrise);
+									$('#sunset').text(sunset);
+									$('#tempAtual').text(temp)
+							  		$('#clickCidade').click();
+							}//else
+						})
+						.fail(function(){
+							alert('erro');
+						})
+				}else{
+					$('#erro').click();
+				}
 			})
-			  
-		  .fail(function() {
-		    alert( "error" );
-		  })
-			
-			
-			$('#nomeCidade').text(cidade['name']);
-
-			$('.modal-trigger').click();
+			  .fail(function() {
+			    alert( "error" );
+			  })	
 		}
 		
 		
@@ -171,7 +206,20 @@ $(document).ready(function(){
 			if (mes.toString().length == 1)
 			  mes = "0"+mes;
 			var ano = data.getFullYear();  
-			return dia+"/"+mes+"/"+ano;
+			return dia+"/"+mes;
+		}
+
+
+		function datahorario(data){
+			hora = ''+data.getHours();
+			min = ''+data.getMinutes();
+			if(hora.length == 1)
+				hora = '0'+hora
+
+			if(min.length == 1)
+				min = '0'+min
+  
+			return hora+":"+min;
 		}
 	    
 	    function toggleBounce() {
